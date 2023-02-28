3862f4344f51acbacd8de2373da6e8a8965e456c
tomaspopik <tpopik1999@gmail.com>
2023-02-28 20:18:36 +0100

    feat: load html DOM

--- app/Console/Commands/ImportNitraDistrictCities.php
+++ app/Console/Commands/ImportNitraDistrictCities.php
@@ -2,7 +2,10 @@
 
 namespace App\Console\Commands;
 
+use DOMDocument;
+use DOMNode;
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
 
 class ImportNitraDistrictCities extends Command
 {
@@ -20,11 +23,36 @@ class ImportNitraDistrictCities extends Command
      */
     protected $description = 'Parse data for all cities located in Nitra district from https://www.e-obce.sk/kraj/NR.html';
 
+    protected DOMDocument $dom;
+
     /**
      * Execute the console command.
      */
-    public function handle(): void
+    public function handle(DOMDocument $dom): void
     {
-        //TODO
+        $this->dom = $dom;
+
+        $url = 'https://www.e-obce.sk/kraj/NR.html';
+        $this->loadHtmlDom($url);
+
+
     }
+
+    private function loadHtmlDom(string $url)
+    {
+        libxml_use_internal_errors(true);
+        if ($this->dom->loadHTML(file_get_contents($url))) {
+            foreach (libxml_get_errors() as $error) {
+                if ($error->level > 2) {
+                    $this->error($error->message);
+                    return;
+                }
+            }
+            libxml_clear_errors();
+        }
+        libxml_use_internal_errors(false);
+
+    }
+
+
 }
