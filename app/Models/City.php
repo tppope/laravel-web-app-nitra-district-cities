d0ab898e190a3142765cb82934cccfb871948e6b
tomaspopik <tpopik1999@gmail.com>
2023-03-01 10:43:16 +0100

    feat: add city model

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class City extends Model
 {
-    use HasFactory;
+
+    protected $guarded = [];
+
+    public function cityHallAddress(): BelongsTo
+    {
+        return $this->belongsTo(Address::class);
+    }
 }
