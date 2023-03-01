9e6e7686a4e49c831d67bd1f9a338e8523b755df
tomaspopik <tpopik1999@gmail.com>
2023-03-01 11:23:49 +0100

    feat: add city import

--- app/Console/Commands/ImportNitraDistrictCities.php
+++ app/Console/Commands/ImportNitraDistrictCities.php
@@ -2,9 +2,13 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Address;
+use App\Models\City;
 use DOMDocument;
 use DOMNode;
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
@@ -43,6 +47,7 @@ class ImportNitraDistrictCities extends Command
 
             $allSubDistrictsCitiesUrls = $this->getAllSubDistrictsCitiesUrls();
 
+            $this->importCities($allSubDistrictsCitiesUrls);
 
         });
     }
@@ -91,10 +96,67 @@ class ImportNitraDistrictCities extends Command
                             ->textContent,
                         '/obec/')
             )
-            ->map(fn(DOMNode $node) => $node->attributes->getNamedItem('href')->textContent);
+            ->mapWithKeys(fn(DOMNode $node) => [
+                trim($node->textContent) => $node->attributes->getNamedItem('href')->textContent
+            ]);
     }
 
+    private function importCities(Collection $citiesUrls)
+    {
+        $citiesUrls->each(function ($cityUrl, $cityName) {
+            $this->loadHtmlDom($cityUrl);
 
+            $this->importCity($cityName);
+
+            return false;
+        });
+    }
+
+    private function importCity(string $cityName)
+    {
+        $editCityUrl = collect($this->dom?->getElementById('telo')?->getElementsByTagName('img')->getIterator())
+            ->first(fn(DOMNode $node) => Str::contains($node->attributes->getNamedItem('src')->textContent, '/images/edit2.jpg'))
+            ->parentNode->attributes->getNamedItem('href')->textContent;
+
+        $this->loadHtmlDom($editCityUrl);
+
+        parse_str(parse_url($editCityUrl)['query'], $query);
+
+        $cityHallAddress = $this->importAddress($cityName);
+
+        City::query()->updateOrCreate(
+            [
+                'id' => $query['id']
+            ],
+            [
+                'name' => $cityName,
+                'mayor_name' => trim($this->dom->getElementById('Starosta')->getAttribute('value')) ?: null,
+                'phone_number' => $this->getPhoneNumber() ?: null,
+                'fax' => trim($this->dom->getElementById('Fax')->getAttribute('value')) ?: null,
+                'web_address' => trim($this->dom->getElementById('URL')->getAttribute('value')) ?: null,
+                'email' => trim($this->dom->getElementById('Email')->getAttribute('value')) ?: null,
+                'city_hall_address_id' => $cityHallAddress->id
+            ]);
+    }
+
+    private function importAddress(string $cityName): Builder|Model
+    {
+        return Address::query()->updateOrCreate([
+            'city_name' => $cityName,
+            'street_name' => trim($this->dom->getElementById('Ulica')->getAttribute('value')),
+            'house_number' => trim($this->dom->getElementById('Cislo')->getAttribute('value')),
+        ], [
+            'postal_code' => trim($this->dom->getElementById('Psc')->getAttribute('value')),
+            'post_name' => trim($this->dom->getElementById('Posta')->getAttribute('value'))
+        ]);
+    }
+
+    private function getPhoneNumber(): string
+    {
+
+        return trim($this->dom->getElementById('SmeroveCislo')->getAttribute('value')) . ' / '
+            . trim($this->dom->getElementById('Telefon')->getAttribute('value'));
+    }
 
 
 }
