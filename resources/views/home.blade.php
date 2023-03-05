3078f70475fb56941fdf8769971296613d94d3b4
tomaspopik <tpopik1999@gmail.com>
2023-03-05 20:57:08 +0100

    feat: add homepage search box

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,10 @@
-<x-layout::main class="bg-primary bg-gradient">
-    AHOJ
+<x-layout::main class="bg-primary bg-gradient d-flex justify-content-center align-items-center flex-grow-1">
+    <section class="container d-flex flex-column justify-content-center align-items-center gap-3 pt-5 pb-5">
+        <header>
+            <h1 id="search-box-h1" class="text-white fw-lighter fs-1">
+                {{ __("Search in the municipality database") }}
+            </h1>
+        </header>
+        <x-search-box/>
+    </section>
 </x-layout::main>
