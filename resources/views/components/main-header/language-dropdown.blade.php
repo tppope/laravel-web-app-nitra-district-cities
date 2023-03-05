8e2c4fc8992a525123439812e9e2a3a2a7fa3139
tomaspopik <tpopik1999@gmail.com>
2023-03-05 20:56:00 +0100

    feat: add main layout header

---
+++ resources/views/components/main-header/language-dropdown.blade.php
@@ -0,0 +1,8 @@
+<div class="dropdown">
+    <button class="btn dropdown-toggle text-secondary" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        SK
+    </button>
+    <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">EN</a></li>
+    </ul>
+</div>
---
+++ resources/views/components/main-header/navbar.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-navbar"
+            aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div id="main-navbar" class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    {{ __("About us") }}
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    {{ __("List of cities") }}
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    {{ __("Inspection") }}
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    {{ __("Contact") }}
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
