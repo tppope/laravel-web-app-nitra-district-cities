a75f8cd8eacd96334ce1be32d862c7f6e941bd45
tomaspopik <tpopik1999@gmail.com>
2023-03-05 20:57:30 +0100

    feat: make the footer always on the bottom of the page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
@@ -15,11 +14,11 @@
     @vite('resources/js/app.js')
 
 </head>
-<body>
+<body class="d-flex flex-column min-vh-100">
 <x-main-header/>
 <main {{ $attributes }}>
     {{ $slot }}
 </main>
-<x-main-footer/>
+<x-main-footer class="mt-auto"/>
 </body>
 </html>
