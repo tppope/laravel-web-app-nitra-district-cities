46c25706d6e175a76ca47d3cc07cec5465a022db
tomaspopik <tpopik1999@gmail.com>
2023-03-07 00:14:21 +0100

    feat: scrollable dropdown

--- resources/views/livewire/search-box.blade.php
+++ resources/views/livewire/search-box.blade.php
@@ -6,7 +6,7 @@
                class="form-control shadow-lg pe-4 ps-4 pt-3 pb-3 dropdown-toggle" aria-label="search">
     </div>
     <div>
-        <ul @class(['dropdown-menu', 'show' => $cities->isNotEmpty()])>
+        <ul @class(['dropdown-menu overflow-auto', 'show' => $cities->isNotEmpty()])>
             @foreach($cities as $id => $cityName)
                 <li><button class="dropdown-item" type="button" wire:click="showCity({{ $id }})">{{ $cityName }}</button></li>
             @endforeach
