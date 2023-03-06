f0ff5d069077a23eff8e1f27418cdda885442135
tomaspopik <tpopik1999@gmail.com>
2023-03-07 00:05:34 +0100

    feat: create new livewire search box component with search autocomplete

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -13,5 +14,10 @@ class City extends Model
     public function cityHallAddress(): BelongsTo
     {
         return $this->belongsTo(Address::class);
+    }
+
+    public function scopeSearch(Builder $query, $cityName): void
+    {
+        $query->where('name', 'LIKE', "%$cityName%");
     }
 }
