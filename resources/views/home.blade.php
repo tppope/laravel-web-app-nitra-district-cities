d66e59bf02bda748908f7296018e007a31710464
tomaspopik <tpopik1999@gmail.com>
2023-03-07 00:17:56 +0100

    fix: center main title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-layout::main class="bg-primary bg-gradient d-flex justify-content-center align-items-center flex-grow-1">
     <section class="container d-flex flex-column justify-content-center align-items-center gap-3 pt-5 pb-5">
         <header>
-            <h1 id="search-box-h1" class="text-white fw-lighter fs-1">
+            <h1 id="search-box-h1" class="text-white text-center fw-lighter fs-1">
                 {{ __("Search in the municipality database") }}
             </h1>
         </header>
